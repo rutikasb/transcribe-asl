75e0f1392ae55ba271d0799c9cf8dfa54f6f4d4a
Chandan Gope <chandangope@gmail.com>
2019-05-30 20:33:36 -0700

    download videos-v2

--- code/xlDownload.py
+++ code/xlDownload.py
@@ -8,8 +8,12 @@ book = openpyxl.load_workbook('../data/dai-asllvd-BU_glossing_with_variations_HS
 
 sheet = book.active
 
+
+#EDIT THE FOLLOWING 2 PARAMS TO DECIDE WHICH ROW OF EXCEL FILE TO START AND END AT FOR DOWNLOADING VIDEOS
 startRow=3
-endRow=5
+endRow=35
+
+
 glossNameCol = 2
 consultantNameCol = 3
 sequenceCol = 13
@@ -33,25 +37,28 @@ def playVideoFile(filename):
 
 
 def downloadVideos():
-	for r in range(startRow, endRow):
+	r = startRow
+	while True:
 		v = sheet.cell(row=r, column=glossNameCol)
 		if(v.value is not None):
 			print("\nMain New Gloss: " + v.value)
-			videoFolderPath = "../data/videos/" + str(v.value)
+			videoFolderPath = "../data/videos/" + str(v.value).replace("/",'-')
 			if not os.path.exists(videoFolderPath):
 				os.makedirs(videoFolderPath)
 
 			print("---------------------")
 			i=r+1
-			consultantStartRow, consultantEndRow = i,i
+			validRows = []
 			while True:
-				vv = sheet.cell(row=i+1, column=consultantNameCol)
+				vv = sheet.cell(row=i, column=consultantNameCol)
 				if(vv.value == "============"):
-					consultantEndRow = i
+					r=i
 					break
+				else:
+					if (vv.value is not None) and (vv.value != '------------'):
+						validRows.append(i)
 				i += 1
-			# print("Consultant start row = {0}, end row = {1}".format(consultantStartRow, consultantEndRow))
-			for i in range(consultantStartRow, consultantEndRow+1):
+			for i in validRows:
 				seq = sheet.cell(row=i, column=sequenceCol)
 				scene = sheet.cell(row=i, column=sceneCol)
 				frameStart = sheet.cell(row=i, column=frameStartCol)
@@ -61,10 +68,11 @@ def downloadVideos():
 				print(url)
 				filename = str(seq.value) + "-" + "scene" + str(scene.value) + "-" + "camera" + cameraNum + "_start" + str(frameStart.value) + "_" + "end" + str(frameEnd.value) + ".mov"
 				filePath = os.path.join(videoFolderPath, filename)
-				print(filePath)
 				urllib.request.urlretrieve(url, filePath)
+		if r >= endRow:
+			break
 
 
 if __name__ == '__main__':
-	# downloadVideos()
-	playVideoFile("/home/chandangope/ucb-mids/w210/transcribe-asl/data/videos/TWENTY/ASL_2008_05_12a-scene1-camera1_start2400_end2480.mov")
+	downloadVideos()
+	# playVideoFile("../data/videos/TWENTY/ASL_2008_05_12a-scene1-camera1_start2400_end2480.mov")
